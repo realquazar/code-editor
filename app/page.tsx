44199dfe4f4274761cbb8eae6bc035778a7a49ab
Quazar <pjohoottoa@gmail.com>
2025-10-03 15:48:13 +0530

    adding authentication

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { db } from "@/lib/db";
+import UserButton from "@/modules/auth/components/user-button";
 import Image from "next/image";
 
 export default async function Home() {
@@ -7,6 +8,7 @@ export default async function Home() {
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
       <Button>Get Started</Button>
+      <UserButton/>      
     </div>
   );
 }
