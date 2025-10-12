becd80c51e48eee2d8dfb0790b1d3b65a24ca301
Quazar <pjohoottoa@gmail.com>
2025-10-12 18:41:22 +0530

    Playground File explorer - 1

---
+++ lib/template.ts
@@ -0,0 +1,8 @@
+export const templatePaths = {
+  REACT: "/frostcode-starters/react-ts",
+  NEXTJS: "/frostcode-starters/nextjs-new",
+  EXPRESS: "/frostcode-starters/express-simple",
+  VUE: "/frostcode-starters/vue",
+  HONO: "/frostcode-starters/hono-nodejs-starter",
+  ANGULAR: "/frostcode-starters/angular",
+};
