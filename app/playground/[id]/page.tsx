ba62cb36d54b9919282f4ebb8fee2e76aab6a959
Quazar <pjohoottoa@gmail.com>
2025-10-31 14:20:16 +0530

    added AI UI

--- app/playground/[id]/page.tsx
+++ app/playground/[id]/page.tsx
@@ -25,11 +25,12 @@ import { useWebContainer } from "@/modules/webcontainers/hooks/useWebContainer";
 import LoadingStep from '@/modules/playground/components/loader';
 import { findFilePath } from '@/modules/playground/lib';
 import { toast } from 'sonner';
+import ToggleAI from '@/modules/playground/components/toggle-ai';
 
 const MainPlaygroundPage = () => {
   const { id } = useParams<{ id: string }>();
 
-  const [isPreviewVisible, setIsPreviewVisible] = useState(false);
+  const [isPreviewVisible, setIsPreviewVisible] = useState(true);
 
   const { playgroundData, templateData, isLoading, error, saveTemplateData } = usePlayground(id);
   const {
@@ -376,9 +377,11 @@ const MainPlaygroundPage = () => {
                   <TooltipContent>Save All(Ctrl+shift+S)</TooltipContent>
                 </Tooltip>
 
-                <Button variant={"default"} size={"icon"}>
-                  <Bot className='size-4'></Bot>
-                </Button>
+                <ToggleAI
+                  isEnabled={false}
+                  onToggle={()=>{}}
+                  suggestionLoading={false}
+                />
 
                 <DropdownMenu>
                   <DropdownMenuTrigger asChild>
