883dbb7fbee1ac651e4040c3b2d7d7fe114aec0b
Quazar <pjohoottoa@gmail.com>
2025-11-02 22:23:32 +0530

    AI chat added completely

--- modules/playground/components/toggle-ai.tsx
+++ modules/playground/components/toggle-ai.tsx
@@ -26,6 +26,7 @@ import {
 } from "lucide-react";
 import React from "react";
 import { cn } from "@/lib/utils";
+import { AIChatSidePanel } from "@/modules/ai-chat/components/ai-chat-sidebarpanel";
 
 
 interface ToggleAIProps {
@@ -181,6 +182,12 @@ const ToggleAI: React.FC<ToggleAIProps> = ({
           </DropdownMenuItem>
         </DropdownMenuContent>
       </DropdownMenu>
+
+<AIChatSidePanel
+isOpen={isChatOpen}
+onClose={() => setIsChatOpen(false)}
+theme="dark"
+/>
 
     </>
   );
